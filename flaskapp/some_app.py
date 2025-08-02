f5655d4a0741f8cf9a67bb2c57525740c0da6338
Анастасия <121298392+anstsa@users.noreply.github.com>
2025-08-02 17:25:00 +0300

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 #декоратор для вывода страницы по умолчанию
 @app.route("/")
 def hello():
- return " <html><head></head> <body> Hello World! </body></html>"
+ return " <html><head></head> <body> Hello World123321! </body></html>"
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000)
